1900a981d75a48f1e2bfa67973ff99be448b4c0a
markmris <mark.mrishih@gmail.com>
2026-02-28 15:46:44 -0400

    Created base screen rendering

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,52 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+void frameBufferSizeCallback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
 int main()
 {
+    // Initialize
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    int width = 1920;
+    int height = 1080;
+
+    GLFWwindow* window = glfwCreateWindow(width, height, "AGE", NULL, NULL);
+
+    if (!window)
+    {
+        std::cout << "Failed to create GLFW window. Terminating program...\n";
+        glfwTerminate();
+
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to load GLAD. Terminating program...";
+
+        return -1;
+    }
     
+    glViewport(0, 0, width, height);
+    glfwSetFramebufferSizeCallback(window, frameBufferSizeCallback);
+
+    // Keep running and handle closing
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
 
     return 0;
 }
